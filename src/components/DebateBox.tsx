99589d2d8d859ff26f183d4203adfd8f22f27bec
Kim <kells@kakao.com>
2023-11-12 14:22:06 +0900

    make debate fit into debatebox

--- src/components/DebateBox.tsx
+++ src/components/DebateBox.tsx
@@ -2,7 +2,7 @@ import Debate from './Debate'
 
 export function DebateBox({ type }: { type: string }) {
   return (
-    <div className='flex gap-2 flex-wrap h-96 max-h-96 overflow-y-auto'>
+    <div className='flex gap-2 flex-wrap h-[408px] max-h-[408px] overflow-y-auto'>
       <Debate
         topic='탕수육 찍먹 vs 부먹'
         description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
