f41673e83291bcf97c1317faeee12d6d123f4ae8
Kim <kells@kakao.com>
2023-11-12 15:21:17 +0900

    i forgot db

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,11 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import jwt from 'jsonwebtoken'
 import { readFileSync } from 'fs'
+import prisma from '@/prisma'
+import { sha256 } from 'js-sha256'
 
 const key = readFileSync('../../private.key').toString()
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { id, pw } = req.body
   if (!(id && pw)) return res.json({ error: 'wrong body', token: null })
-  res.json({ token: jwt.sign({ id }, key) })
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!user) return res.json({ error: 'wrong id', token: null })
+  if (sha256(pw + user.salt) === user.pw)
+    return res.json({ token: jwt.sign({ id }, key) })
 }
