4a51f59580e8163903a614ba8cb1d2fe347df713
Kim <kells@kakao.com>
2023-11-12 14:43:30 +0900

    change to korean

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ export default function TopBar() {
     <div className='flex items-center mb-[-12px]'>
       <img src='logo-full.svg' width={175} height={50.92} className='p-4' />
       <a className='font-bold text-blue-500 ml-[-5px]' href='/login'>
-        Login
+        로그인
       </a>
     </div>
   )
