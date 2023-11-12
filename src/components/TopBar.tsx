e82ec1fe84c81d926c42877c6ce5fe5a27180fae
Kim <kells@kakao.com>
2023-11-12 14:54:24 +0900

    add href to main page

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,9 @@
 export default function TopBar() {
   return (
     <div className='flex items-center mb-[-12px]'>
-      <img src='logo-full.svg' width={175} height={50.92} className='p-4' />
+      <a href='/'>
+        <img src='logo-full.svg' width={175} height={50.92} className='p-4' />
+      </a>
       <a className='font-bold text-blue-500 ml-[-5px]' href='/login'>
         로그인
       </a>
