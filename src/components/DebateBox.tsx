40f06fc0e58e0960a38752d8ab8e715473dffe9b
Kim <kells@kakao.com>
2023-11-14 18:07:41 +0900

    implement debate list

--- src/components/DebateBox.tsx
+++ src/components/DebateBox.tsx
@@ -1,99 +1,55 @@
+import Room from '@/interfaces/Room'
 import Debate from './Debate'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export function DebateBox({ type }: { type: string }) {
+  const {
+    data: rooms,
+    error,
+    isLoading,
+  }: {
+    data: Room[] | undefined
+    error: Error | undefined
+    isLoading: boolean
+  } = useSWR('/api/debates', fetcher)
+
+  if (error) return <div>error</div>
+
   return (
     <div className='flex gap-2 flex-wrap h-[408px] max-h-[408px] overflow-y-auto'>
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
-      <Debate
-        topic='탕수육 찍먹 vs 부먹'
-        description='탕수육 찍먹하는지 부먹하는지 토론을 진행하겠습니다.'
-        ownerId='kellsthepenguin'
-        minutes={50}
-        date={new Date()}
-      />
+      {!isLoading ? (
+        rooms!.map((room) => {
+          if (type === 'recruiting') {
+            if (room.isOngoing === false) {
+              return (
+                <Debate
+                  topic={room.topic}
+                  description={room.description}
+                  ownerId={room.owner.id}
+                  minutes={room.time}
+                  date={new Date()}
+                />
+              )
+            }
+          } else {
+            if (room.isOngoing === true) {
+              return (
+                <Debate
+                  topic={room.topic}
+                  description={room.description}
+                  ownerId={room.owner.id}
+                  minutes={room.time}
+                  date={new Date()}
+                />
+              )
+            }
+          }
+        })
+      ) : (
+        <div>loading</div>
+      )}
     </div>
   )
 }
