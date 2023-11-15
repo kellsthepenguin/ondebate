187f00b50017960b82cd2652b765e3d3cf77f26a
Kim <kells@kakao.com>
2023-11-15 13:43:21 +0900

    aaaaaa

--- src/components/DebatePage.tsx
+++ src/components/DebatePage.tsx
@@ -1,11 +1,53 @@
+import Room from '@/interfaces/Room'
 import TopBar from './TopBar'
 import { Socket } from 'socket.io-client'
+import { useEffect } from 'react'
 
-export default function DebatePage({ socket }: { socket: Socket }) {
+export default function DebatePage({
+  socket,
+  room,
+}: {
+  socket: Socket
+  room: Room
+}) {
   return (
     <div>
       <TopBar />
-      TODO
+      <p className='p-5 text-3xl font-bold'>{room.topic}</p>
+      <div className='flex flex-row gap-5'>
+        <div className='pl-5'>
+          <p className='mt-3 mb-2 text-3xl font-bold text-red-500'>
+            {room.groups[0]}
+          </p>
+          <div className='w-[30rem] h-[50rem] outline outline-gray-400 rounded-md p-5'>
+            {room.users.map((user) => {
+              if (user.group === room.groups[0]) {
+                return (
+                  <div className='outline outline-black p-2 text-red-500 w-40'>
+                    {user.id}
+                  </div>
+                )
+              }
+            })}
+          </div>
+        </div>
+        <div>
+          <p className='mt-3 mb-2 text-3xl font-bold text-blue-500'>
+            {room.groups[1]}
+          </p>
+          <div className='w-[30rem] h-[50rem] outline outline-gray-400 rounded-md p-5'>
+            {room.users.map((user) => {
+              if (user.group === room.groups[1]) {
+                return (
+                  <div className='outline outline-black p-2 text-blue-500 w-40'>
+                    {user.id}
+                  </div>
+                )
+              }
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
