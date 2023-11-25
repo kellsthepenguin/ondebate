462f5f464b6ef1bf228261116b3fc79cad4e2e56
Kim <kells@kakao.com>
2023-11-25 16:32:03 +0900

    remove time in createdebate

--- src/components/CreateDebate.tsx
+++ src/components/CreateDebate.tsx
@@ -46,7 +46,7 @@ export default function CreateDebate({ socket }: { socket: Socket }) {
   }
 
   return (
-    <div className='w-96 h-[32rem] bg-gray-200 rounded-md p-5'>
+    <div className='w-96 h-[29rem] bg-gray-200 rounded-md p-5'>
       <p className='text-3xl font-bold'>새 토론 만들기</p> <br />
       <p className='font-semibold'>논제</p>
       <Input type='text' placeholder='짜장면 vs 짬뽕' innerRef={topicRef} />
@@ -59,9 +59,12 @@ export default function CreateDebate({ socket }: { socket: Socket }) {
       <p className='pt-2 font-semibold'>1번 집단</p>
       <Input type='text' placeholder='짜장면' innerRef={firstGroupRef} />
       <p className='pt-2 font-semibold'>2번 집단</p>
-      <Input type='text' placeholder='짬뽕' innerRef={secondGroupRef} />
-      <p>시간(분)</p>
-      <Input type='number' placeholder='50' innerRef={timeRef} /> <br />
+      <Input
+        type='text'
+        placeholder='짬뽕'
+        innerRef={secondGroupRef}
+        className='mb-10'
+      />
       <PrimaryButton onClick={handleOnClick}>토론 만들기</PrimaryButton>
     </div>
   )
